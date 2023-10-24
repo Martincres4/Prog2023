0f9b58113fcd18b7a2d2c32164898d1838d01940
Martincres4 <martin.rdp2000@gmail.com>
2023-10-24 17:17:55 +0200

    24/10

--- Martín Crespo DAM/Programación/1Trimestre/condicionales/opcional3.java
+++ Martín Crespo DAM/Programación/1Trimestre/condicionales/opcional3.java
@@ -9,12 +9,15 @@ public class opcional3 {
         System.out.println("Introduce un lado del triángulo: ");
         String lado1S = sc.nextLine();
         int lado1 = Integer.parseInt(lado1S);
+
         System.out.println("Introduce otro lado del triángulo: ");
         String lado2S = sc.nextLine();
         int lado2 = Integer.parseInt(lado2S);
+        
         System.out.println("Introduce ul tercer lado del triángulo: ");
         String lado3S = sc.nextLine();
         int lado3 = Integer.parseInt(lado3S);
+
         if(lado1==lado2 && lado2==lado3){
             System.out.println("El triángulo es equilátero.");
         }else if(lado1==lado2 || lado2==lado3){
--- Martín Crespo DAM/Programación/1Trimestre/ifElseIfAnidados/ejer4.java
+++ Martín Crespo DAM/Programación/1Trimestre/ifElseIfAnidados/ejer4.java
@@ -1,41 +1,51 @@
 /*Pide un numero, si es par, debera sumarle 10(de lo contrario sumar 1), si es mayor que 
 500, le restara 10, ademas  si es divisible entre 5 multiplicarlo por 3*/
 package ifElseIfAnidados;
+
 import java.util.Scanner;
+
 public class ejer4 {
     public static void main(String[] args) {
+        boolean par = false;
+        boolean mayor = false;
+        boolean divisible = false;
+        String numS;
+        int num;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número: ");
-        String numS = sc.nextLine();
-        int num = Integer.parseInt(numS);
-        boolean par= true; 
-        boolean mayor= true;
-        boolean divisible= true; 
+        numS = sc.nextLine();
+        num = Integer.parseInt(numS);
+        
+        
 
-        if(num%2==0){
-            par=true;
+        if (num % 2 == 0) {
+            par = true;
         }
-        if(num>500){
-            mayor=true;
+        if (num > 500) {
+            mayor = true;
         }
-        if(num%5==0){
-            divisible=true;
+        if (num % 5 == 0) {
+            divisible = true;
         }
 
-        if(par){
-            num+=10;
-        }else{
-            num+=1;
-        }
-        if(mayor){
-            num-=10;
-        }
-        if(divisible){
-            num+=num*3;
+        if (par) {
+            num += 10;
+            if (mayor) {
+                num -= 10;
+            }
+            if (divisible) {
+                num = num * 3;
+            }
+        } else {
+            num = num + 1;
+            if (mayor) {
+                num -= 10;
+            }
+            if (divisible) {
+                num *= 3;
+            }
         }
         System.out.println(num);
     }
 }
-
-    
-
