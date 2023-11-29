3e828005f486a8d2f6ddb6930d22429d7d17ddd7
Martincres4 <martin.rdp2000@gmail.com>
2023-11-29 19:31:41 +0100

    29/11

---
+++ practicar/condicionales.java
@@ -0,0 +1,8 @@
+package practicar;
+
+public class condicionales {
+    public static void main (String[]args){
+
+    }
+    
+}
---
+++ practicar/variables.java
@@ -0,0 +1,16 @@
+package practicar;
+
+public class variables {
+    public static void main(String[]args){
+        boolean b = true;
+
+        //boolean toBe = false;
+       
+        if (b) {
+            System.out.println("no");
+        
+        }else{
+            System.out.println("si");
+        }
+    }
+}
