45d7f4ab6925f0d2747f2304437b1b79dd8cdd24
Martincres4 <martin.rdp2000@gmail.com>
2023-12-03 22:40:40 +0100

    Create ej1.java

---
+++ Martín Crespo DAM/ejsNot/ej1.java
@@ -0,0 +1,13 @@
+public class ej1 {
+    public static void main(String[]Args){
+        
+        //Ejemplos :
+        int i = 7, j = 9 ;
+        System.out.println( "Prueba operadores Logicos : " + (i > j) );
+        System.out.println( "Prueba operadores Logicos : " + (i <= j) );
+        System.out.println( "Prueba operadores Logicos : " + (i != j) );
+        System.out.printf( "%2d es mayor que %d : es %b\n", i, j, (i > j) );
+
+
+    }    
+}
