0b77779c560f7594704cb28dac5782be5d1410c6
Martincres4 <martin.rdp2000@gmail.com>
2023-12-12 19:13:42 +0100

    12/12/2

--- Martín Crespo DAM/Programación/2Trimestre/clasesYObjetos/clasesYObjetos/animal.java
+++ Martín Crespo DAM/Programación/2Trimestre/clasesYObjetos/clasesYObjetos/animal.java
@@ -1,5 +1,25 @@
-package Martín Crespo DAM.Programación.2Trimestre.clasesYObjetos;
+//Crear la clase animal que tiene los siguientes atributos: nombre,tamaño, peso, subespecie y tiene como método comer,reproduccion y matar.
+public class Animal {
+    private String nombre, subespecie;
+    private float tamano, peso;
 
-public class animal {
     
+    
+    public Animal(String nombre, float tamano, float peso, String subespecie){
+        this.nombre = nombre;
+        this.tamano = tamano;
+        this.peso = peso;
+        this.subespecie = subespecie;
+    }
+
+    
+    prop
+    public static void comer(){
+
+    }
+    public static void reproduccion(){
+
+    }
+    public static void matar(){
+    }
 }
---
+++ Martín Crespo DAM/Programación/2Trimestre/clasesYObjetos/clasesYObjetos/main.java
@@ -0,0 +1,8 @@
+/*
+Crear la clase persona que tiene los siguientes atributos: nombre, apellidos, edad, localizacion, codigo postal, dni
+Crear la clase trabajador que tiene los siguientes atributos: nombre, puesto de trabajo, departamento(solo existe el departamento de informatica y recursos humanos), sueldo(ha de ser positivo), nombreJefe
+Crear la clase Coche que tiene los siguientes atributos: nombre, modelo, marca, peso, precio. Crear 3 objetos con diferentes valores y mostrarlos por pantalla.
+ */
+public class main {
+    
+}
