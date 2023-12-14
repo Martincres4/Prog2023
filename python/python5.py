fcca53ad8e14ca1928b2688140ab4c4a517fee31
Martincres4 <martin.rdp2000@gmail.com>
2023-12-15 00:35:08 +0100

    12/12

--- python/python5.py
+++ python/python5.py
@@ -211,4 +211,51 @@ else:
     print("We need to find some users!")
 
 
+"""5-10. Checking Usernames: Do the following to create a program that simulates 
+how websites ensure that everyone has a unique username.
+•	 Make a list of five or more usernames called current_users.
+•	 Make another list of five usernames called new_users. Make sure one or 
+two of the new usernames are also in the current_users list.
+•	 Loop through the new_users list to see if each new username has already 
+been used. If it has, print a message that the person will need to enter a 
+new username. If a username has not been used, print a message saying 
+that the username is available.
+•	 Make sure your comparison is case insensitive. If 'John' has been used, 
+'JOHN' should not be accepted."""
+current_users = ['manolo', 'grekas', 'admin', 'martins', 'tusmu']
+new_usernames = ['manolo', 'si33', 'admin', 'kakaolat', 'tusmu']
+for new_username in new_usernames:
+    if new_username in current_users:
+        print("Nombre no disponible locuelo")
+    else:
+        print("nombre disponible ")
 
+
+"""5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such 
+as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+•	 Store the numbers 1 through 9 in a list.
+•	 Loop through the list.
+•	 Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 
+7th 8th 9th", and each result should be on a separate line."""
+nums = [1,2,3,4,5,6,7,8,9]
+for num in nums:
+    if num == 1:
+        print(num," st")
+    elif num == 2:
+        print(num," nd")
+    elif num == 3:
+        print(num, " thrd")
+    else:
+        print(num, " nd")
+
+
+"""5-12. Styling if statements: Review the programs you wrote in this chapter, and 
+make sure you styled your conditional tests appropriately."""
+
+
+"""5-13. Your Ideas: At this point, you’re a more capable programmer than you 
+were when you started this book. Now that you have a better sense of how 
+real-world situations are modeled in programs, you might be thinking of some 
+problems you could solve with your own programs. Record any new ideas you 
+have about problems you might want to solve as your programming skills continue to improve. Consider games you might want to write, data sets you might 
+want to explore, and web applications you’d like to create"""
---
+++ python/python6.py
@@ -0,0 +1,130 @@
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+new_points = alien_0['points']
+#cambia a string el str
+print("You just earned " + str(new_points) + " points!")
+
+#empty
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+#swap
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
+
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
+
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+# The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
+#borrar datos
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+
+print("Sarah's favorite language is " +
+favorite_languages['sarah'].title() +".")
+
+
+"""6-1. Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You 
+should have keys such as first_name, last_name, age, and city. Print each 
+piece of information stored in your dictionary."""
+
+
+"""6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite 
+number for each person, and store each as a value in your dictionary. Print 
+each person’s name and their favorite number. For even more fun, poll a few 
+friends and get some actual data for your program."""
+
+
+
+"""6-3. Glossary: A Python dictionary can be used to model an actual dictionary.
+However, to avoid confusion, let’s call it a glossary.
+•	 Think of five programming words you’ve learned about in the previous 
+chapters. Use these words as the keys in your glossary, and store their 
+meanings as values.
+•	 Print each word and its meaning as neatly formatted output. You might 
+print the word followed by a colon and then its meaning, or print the word 
+on one line and then print its meaning indented on a second line. Use the 
+newline character (\n) to insert a blank line between each word-meaning 
+pair in your output."""
+
+
+#looping dictionary
+user_0 = {
+ 'username': 'efermi',
+ 'first': 'enrico',
+ 'last': 'fermi',
+ }
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + ".")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+ 
+    if name in friends:
+        print(" Hi " + name.title() +
+            ", I see your favorite language is " +
+            favorite_languages[name].title() + "!")
+
+
+
+for name in sorted(favorite_languages.keys()):
+ print(name.title() + ", thank you for taking the poll.")
+
+
+
+
+
+
+
+
