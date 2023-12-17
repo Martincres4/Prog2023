900702273381201f37acf8d0483b97166b441be0
Martincres4 <martin.rdp2000@gmail.com>
2023-12-17 23:59:01 +0100

    Update python5.py

--- python/python5.py
+++ python/python5.py
@@ -244,18 +244,8 @@ for num in nums:
     elif num == 2:
         print(num," nd")
     elif num == 3:
-        print(num, " thrd")
+        print(num, " rd")
     else:
-        print(num, " nd")
+        print(num, " th")
 
 
-"""5-12. Styling if statements: Review the programs you wrote in this chapter, and 
-make sure you styled your conditional tests appropriately."""
-
-
-"""5-13. Your Ideas: At this point, you’re a more capable programmer than you 
-were when you started this book. Now that you have a better sense of how 
-real-world situations are modeled in programs, you might be thinking of some 
-problems you could solve with your own programs. Record any new ideas you 
-have about problems you might want to solve as your programming skills continue to improve. Consider games you might want to write, data sets you might 
-want to explore, and web applications you’d like to create"""
