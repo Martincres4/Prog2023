442fda4659dbf592208ff03a798b6e37bf91ac6c
Martincres4 <martin.rdp2000@gmail.com>
2023-12-19 01:28:55 +0100

    Update python6.py

--- python/python6.py
+++ python/python6.py
@@ -118,15 +118,185 @@ for name in favorite_languages.keys():
             ", I see your favorite language is " +
             favorite_languages[name].title() + "!")
 
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
 
 
 for name in sorted(favorite_languages.keys()):
     print(name.title() + ", thank you for taking the poll.")
 
 
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+"""6-4. Glossary 2: Now that you know how to loop through a dictionary, clean
+up the code from Exercise 6-3 (page 102) by replacing your series of print
+statements with a loop that runs through the dictionary’s keys and values.
+When you’re sure that your loop works, add five more Python terms to your
+glossary. When you run your program again, these new words and meanings
+should automatically be included in the output."""
+
+
+"""6-5. Rivers: Make a dictionary containing three major rivers and the country
+each river runs through. One key-value pair might be 'nile': 'egypt'.
+•	 Use a loop to print a sentence about each river, such as The Nile runs
+through Egypt.
+•	 Use a loop to print the name of each river included in the dictionary.
+•	 Use a loop to print the name of each country included in the dictionary."""
+
+
+"""6-6. Polling: Use the code in favorite_languages.py (page 104).
+•	 Make a list of people who should take the favorite languages poll. Include
+some names that are already in the dictionary and some that are not.
+•	 Loop through the list of people who should take the poll. If they have
+already taken the poll, print a message thanking them for responding.
+If they have not yet taken the poll, print a message inviting them to take
+the poll."""
 
 
 
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+
+
+for alien in aliens:
+    print(alien)
+
+
+# Make an empty list for storing aliens.
+aliens = []
+# Make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# Show the first 5 aliens:
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
+
+
+# Make an empty list for storing aliens.
+aliens = []
+# Make 30 green aliens.
+for alien_number in range (0,30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# Show the first 5 aliens:
+for alien in aliens[0:5]:
+    print(alien)
+print("...")
 
 
 
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+# Store information about a pizza being ordered.
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+    }
+# Summarize the order.
+print("You ordered a " + pizza['crust'] + "-crust pizza " +
+    "with the following toppings:")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+        },
+
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        },
+    }
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
+
+"""6-7. People: Start with the program you wrote for Exercise 6-1 (page 102).
+Make two new dictionaries representing different people, and store all three
+dictionaries in a list called people. Loop through your list of people. As you
+loop through the list, print everything you know about each person."""
+
+"""6-8. Pets: Make several dictionaries, where the name of each dictionary is the
+name of a pet. In each dictionary, include the kind of animal and the owner’s
+name. Store these dictionaries in a list called pets. Next, loop through your list
+and as you do print everything you know about each pet."""
+
+"""6-9. Favorite Places: Make a dictionary called favorite_places. Think of three
+names to use as keys in the dictionary, and store one to three favorite places
+for each person. To make this exercise a bit more interesting, ask some friends
+to name a few of their favorite places. Loop through the dictionary, and print
+each person’s name and their favorite places."""
+
+"""6-10. Favorite Numbers: Modify your program from Exercise 6-2 (page 102) so
+each person can have more than one favorite number. Then print each person’s
+name along with their favorite numbers."""
+
+"""6-11. Cities: Make a dictionary called cities. Use the names of three cities as
+keys in your dictionary. Create a dictionary of information about each city and
+include the country that the city is in, its approximate population, and one fact
+about that city. The keys for each city’s dictionary should be something like
+country, population, and fact. Print the name of each city and all of the information you have stored about it."""
+
+"""6-12. Extensions: We’re now working with examples that are complex enough
+that they can be extended in any number of ways. Use one of the example programs from this chapter, and extend it by adding new keys and values, changing the context of the program or improving the formatting of the output."""
