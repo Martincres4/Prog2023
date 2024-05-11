d181fd8c10a19344c422ed7f853cff184120a4d0
Martincres4 <martin.rdp2000@gmail.com>
2024-05-11 16:09:49 +0200

    Update python6.py

--- python/python6.py
+++ python/python6.py
@@ -79,7 +79,13 @@ Think of five names, and use them as keys in your dictionary. Think of a favorit
 number for each person, and store each as a value in your dictionary. Print 
 each person’s name and their favorite number. For even more fun, poll a few 
 friends and get some actual data for your program."""
-
+favourite_numbers = {
+    'Angel': 23,
+    'Alvaro': 24,
+    'Carmen': 3,
+    'Lucia': 33,
+    'Susi': 4,
+}
 
 
 """6-3. Glossary: A Python dictionary can be used to model an actual dictionary.
